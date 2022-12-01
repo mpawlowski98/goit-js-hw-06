e3e75784ee2f54ce111020e6cdbdc67637d5ca03
Michał Pawłowski <107926477+mpawlowski98@users.noreply.github.com>
2022-12-01 21:32:39 +0100

    4 zad

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,24 @@
+"use scrict";
 
+const counter = {
+  counterValue: 0,
+  htmlObij: document.querySelector("#value"),
+  updateCounter() {
+    this.htmlObij.textContent = this.counterValue;
+  },
+  downCounter() {
+    this.counterValue--;
+    this.updateCounter();
+  },
+  upCounter() {
+    this.counterValue++;
+    this.updateCounter();
+  },
+};
+
+document
+  .querySelector(`button[data-action="increment"]`)
+  .addEventListener("mouseup", counter.upCounter.bind(counter));
+document
+  .querySelector(`button[data-action="decrement"]`)
+  .addEventListener("mouseup", counter.downCounter.bind(counter));
