e8cbe723aa925848adb39933dcda2c3bf03b97bd
Michał Pawłowski <107926477+mpawlowski98@users.noreply.github.com>
2022-12-12 12:05:27 +0100

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,4 +1,50 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 // In During
+
+function getOfBoxes() {
+  const value = document.querySelector('* [type="number"]').value;
+  const max = Number(
+    document.querySelector('* [type="number"]').getAttribute("max")
+  );
+  const min = Number(
+    document.querySelector('* [type="number"]').getAttribute("min")
+  );
+  if (value === "") return 0;
+  if (value < min) return min;
+  if (value > max) return max;
+  return value;
+}
+
+function createBoxes() {
+  const target = document.querySelector("#boxes");
+  const boxes = [];
+  for (
+    let i = target.childElementCount;
+    i < +getOfBoxes() + target.childElementCount;
+    i++
+  ) {
+    const box = document.createElement("div");
+    box.style.width = 30 + i * 10 + "px";
+    box.style.height = 30 + i * 10 + "px";
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+  target.append(...boxes);
+}
+
+function destroyBoxes() {
+  document.querySelector("#boxes").innerHTML = "";
+}
+
+document.querySelector("[data-create]").addEventListener("click", createBoxes);
+document
+  .querySelector("[data-destroy]")
+  .addEventListener("click", destroyBoxes);
